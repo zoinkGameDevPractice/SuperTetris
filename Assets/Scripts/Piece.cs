de313c1899f612b3a34f028f2c07fbac1e185201
zoinkGameDevPractice <zoinkGameDevPractice@users.noreply.github.com>
2020-06-05 16:23:21 -0400

    Finished movement

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -4,39 +4,123 @@ using UnityEngine;
 
 public class Piece : MonoBehaviour
 {
-    float fallSpeed = 0.8f;
+    public float fallSpeed = 0.7f;
+    public float fallBoost = 0.6f;
     float fallTimer;
+    float newFallSpeed;
+    bool accelerate = false;
 
+    public Vector3 rotatePoint;
+
+    public float moveDelay = 0.1f;
     float move;
-    public float moveDelay = 0.4f;
     float moveTimer;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    static int height = 20;
+    static int width = 10;
 
     // Update is called once per frame
     void Update()
     {
+        Timers();
+        InputCalculation();
+        MoveCalculation();
+        FallCalculation();
+    }
+
+    void Fall()
+    {
+        transform.position += new Vector3(0, -1f);
+        if(!ValidMove())
+        {
+            transform.position -= new Vector3(0, -1f);
+        }
+    }
+
+    bool ValidMove()
+    {
+        foreach(Transform child in transform)
+        {
+            if(Mathf.RoundToInt(child.position.x) >= width || Mathf.RoundToInt(child.position.x) < 0)
+            {
+                return false;
+            } else if(Mathf.RoundToInt(child.position.y) < 0 || Mathf.RoundToInt(child.position.y) >= height)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    void InputCalculation()
+    {
         move = Input.GetAxisRaw("Horizontal");
-        moveTimer += Time.deltaTime;
-        fallTimer += Time.deltaTime;
-        if(move != 0 && moveTimer > moveDelay)
+
+        if (Input.GetAxisRaw("Vertical") == -1)
+            accelerate = true;
+
+        else
+            accelerate = false;
+
+        if (Input.GetKeyDown(KeyCode.K))
+            Rotate(0);
+        if (Input.GetKeyDown(KeyCode.L))
+            Rotate(1);
+    }
+
+    void MoveCalculation()
+    {
+        if (move != 0 && moveTimer > moveDelay)
         {
             transform.position += new Vector3(move, 0);
+            if (!ValidMove())
+            {
+                transform.position -= new Vector3(move, 0);
+            }
             moveTimer = 0;
         }
-        if(fallTimer > fallSpeed)
+    }
+
+    void FallCalculation()
+    {
+        if (accelerate)
+        {
+            newFallSpeed = fallSpeed - fallBoost;
+        }
+        else
+            newFallSpeed = fallSpeed;
+        if (fallTimer > newFallSpeed)
         {
             Fall();
             fallTimer = 0;
         }
     }
 
-    void Fall()
+    void Rotate(int dir)
     {
-        transform.position += new Vector3(0, -1f);
+        if(dir == 0)
+        {
+            transform.RotateAround(transform.TransformPoint(rotatePoint), new Vector3(0, 0, 1), -90f);
+            if(!ValidMove())
+            {
+                transform.RotateAround(transform.TransformPoint(rotatePoint), new Vector3(0, 0, 1), 90f);
+            }
+        }
+
+        if(dir == 1)
+        {
+            transform.RotateAround(transform.TransformPoint(rotatePoint), new Vector3(0, 0, 1), 90f);
+            if(!ValidMove())
+            {
+                transform.RotateAround(transform.TransformPoint(rotatePoint), new Vector3(0, 0, 1), -90f);
+            }
+        }
+    }
+
+    void Timers()
+    {
+        moveTimer += Time.deltaTime;
+        fallTimer += Time.deltaTime;
     }
 }
